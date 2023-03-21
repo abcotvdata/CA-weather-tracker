f980f5442f61f7c543c675ffb43dd5fc7916abb9
lindseyf96 <39309451+lindseyf96@users.noreply.github.com>
2023-03-21 16:38:39 -0700

    Update precip_scraper.py

--- scripts/precip_scraper.py
+++ scripts/precip_scraper.py
@@ -4,6 +4,7 @@ import time
 import urllib
 import re
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import selenium.webdriver.common.keys
