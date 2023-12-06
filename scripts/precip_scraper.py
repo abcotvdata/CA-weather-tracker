db1222c42f1901293afeb8645f2cad7907006133
lindseyf96 <39309451+lindseyf96@users.noreply.github.com>
2023-12-05 17:35:00 -0800

    Update precip_scraper.py

--- scripts/precip_scraper.py
+++ scripts/precip_scraper.py
@@ -20,7 +20,7 @@ from selenium.webdriver.chrome.service import Service
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
-options.add_argument("-headless")
+chrome_options.add_argument("-headless")
 options = [
     "--headless",
     "--disable-gpu",
