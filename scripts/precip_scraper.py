a60a3690e735206eb1ef861a0f4b46a747588ef8
lindseyf96 <39309451+lindseyf96@users.noreply.github.com>
2023-12-05 17:31:03 -0800

    Update precip_scraper.py

--- scripts/precip_scraper.py
+++ scripts/precip_scraper.py
@@ -20,8 +20,9 @@ from selenium.webdriver.chrome.service import Service
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
+options.add_argument("-headless")
 options = [
-    "--headless=new",
+    "--headless",
     "--disable-gpu",
     "--window-size=1920,1200",
     "--ignore-certificate-errors",
