265bd7c86d47ac7a5db9fa2a5c12ded9348c90c7
lindseyf96 <39309451+lindseyf96@users.noreply.github.com>
2026-07-16 11:10:47 -0400

    Update precip_scraper.py

--- scripts/precip_scraper.py
+++ scripts/precip_scraper.py
@@ -12,12 +12,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 #from webdriver_manager.core.utils import ChromeType
-from webdriver_manager.core.os_manager import ChromeType
+#from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+#chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+chrome_service = Service(ChromeDriverManager().install())
 
 chrome_options = Options()
 
